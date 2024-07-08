b57c82d6975c5ecc34f475b6500516eea22cd87d
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2024-07-09 03:11:21 +0530

    RecyclerView Basics

---
+++ app/src/main/java/com/example/rwcyclerview/Model/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.rwcyclerview.Model
+
+class Constants{
+    companion object {
+        const val Key_tittle:String = "title"
+         const val Key_value:String = "desc"
+    }
+}
